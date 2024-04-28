7e132b80468bcb32c818bc87c606551e622c4a61
MadMach420 <mach.krzsztf@gmail.com>
2024-04-28 18:35:43 +0200

    Update DeckInfo.cpp

--- Source/NICGame/Private/DeckInfo.cpp
+++ Source/NICGame/Private/DeckInfo.cpp
@@ -7,20 +7,13 @@ void UDeckInfo::CreateMockDeck()
 {
 	if (this->SinglePlayerDeck.Num() == 0)
 	{
-		UCardData* Data1 = NewObject<UCardData>();
-		Data1->SetData("Test1", "Test1");
-		Data1->AddToRoot();
-		this->SinglePlayerDeck.Add(Data1);
-
-		UCardData* Data2 = NewObject<UCardData>();
-		Data2->SetData("Test2", "Test2");
-		Data2->AddToRoot();
-		this->SinglePlayerDeck.Add(Data2);
-
-		UCardData* Data3 = NewObject<UCardData>();
-		Data3->SetData("Test3", "Test3");
-		Data3->AddToRoot();
-		this->SinglePlayerDeck.Add(Data3);
+		for (int i = 0; i < 5; i++) 
+		{
+			UCardData* Data = NewObject<UCardData>();
+			Data->SetData(std::to_string(i), std::to_string(i));
+			Data->AddToRoot();
+			this->SinglePlayerDeck.Add(Data);
+		}
 	}
 }
 
@@ -30,10 +23,9 @@ TArray<UCardData*> UDeckInfo::GetSinglePlayerDeck()
 	{
 		this->CreateMockDeck();
 	}
+
 	for (int i = 0; i < this->SinglePlayerDeck.Num(); i++)
 	{
-		// Nie wiem co tu sie dzieje
-		// https://forums.unrealengine.com/t/duplicate-uobject-into-another-one/311521/4
 		this->DeckCopy.Add(DuplicateObject(this->SinglePlayerDeck[i], this->GetOuter()));
 	}
 	return DeckCopy;
