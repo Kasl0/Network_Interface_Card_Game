8ab230a67dc9e2b0d602809326f492922044161b
MadMach420 <mach.krzsztf@gmail.com>
2024-05-15 20:12:19 +0200

    mode change when sitting down, card hover

--- Source/NICGame/Private/CardHandWidget.cpp
+++ Source/NICGame/Private/CardHandWidget.cpp
@@ -3,15 +3,14 @@
 
 #include "CardHandWidget.h"
 #include <iostream>
-#include "CardBattleCharacter.h"
 #include "Kismet/GameplayStatics.h"
 
 void UCardHandWidget::DrawCard()
 {
 	if (!this->BattleDeck) 
 	{
-		ACardBattleCharacter* Ab = Cast<ACardBattleCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
-		this->BattleDeck = Ab->GetBattleDeck();
+		this->BattleDeck = NewObject<UBattleDeck>();
+		this->BattleDeck->AddToRoot();
 		this->BattleDeck->InitializeDeck(GetWorld()); 
 	}
 	UCardData* NewCardData = this->BattleDeck->DrawCard();
